014b76d9693688f5e68ebd1206afa9ce5491fc4f
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-10-13 10:42:53 +0300

    Update

---
+++ custom_stl_c/include/custom/_c_basic_string.h
@@ -0,0 +1,17 @@
+#ifndef _C_BASIC_STRING_H
+#define _C_BASIC_STRING_H
+
+
+#include "custom/c_utility.h"
+
+
+#define _DEFINE_BASIC_STRING(               \
+    BASIC_STRING_PUBLIC_PREFIX,             \
+    CHAR_TYPE                               \
+)                                           \
+
+
+
+
+
+#endif  // _C_BASIC_STRING_H
