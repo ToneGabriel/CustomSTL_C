03de37ef03825d8852cfcc24514726054a794a68
ToneGabriel <81569579+ToneGabriel@users.noreply.github.com>
2025-10-26 10:51:01 +0200

    Update

--- custom_stl_c/include/custom/_c_char_traits.h
+++ custom_stl_c/include/custom/_c_char_traits.h
@@ -20,9 +20,9 @@ static size_t               _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, length)(co
 static const CHAR_TYPE*     _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, find)(const CHAR_TYPE* str, size_t count, CHAR_TYPE chr);                                                             \
 static const CHAR_TYPE*     _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, assign)(CHAR_TYPE* const str, size_t count, CHAR_TYPE chr);                                                           \
                                                                                                                                                                                             \
-static int                  _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_compare)(const CHAR_TYPE* str1, size_t pos1, size_t len1, const CHAR_TYPE* str2, size_t pos2, size_t len2);    \
-static size_t               _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_find)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len);                                   \
-static size_t               _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_rfind)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len);                                  \
+static int                  _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, compare_substr)(const CHAR_TYPE* str1, size_t pos1, size_t len1, const CHAR_TYPE* str2, size_t pos2, size_t len2);    \
+static size_t               _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, find_substr)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len);                                   \
+static size_t               _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, rfind_substr)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len);                                  \
                                                                                                                                                                                             \
 static CHAR_TYPE* _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, copy)(CHAR_TYPE* const dest, const CHAR_TYPE* const source, size_t count)                                                       \
 {                                                                                                                                                                                           \
@@ -60,7 +60,7 @@ static const CHAR_TYPE* _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, assign)(CHAR_T
     return str;                                                                                                                                                                             \
 }                                                                                                                                                                                           \
                                                                                                                                                                                             \
-static int _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_compare)(const CHAR_TYPE* str1, size_t pos1, size_t len1, const CHAR_TYPE* str2, size_t pos2, size_t len2)                      \
+static int _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, compare_substr)(const CHAR_TYPE* str1, size_t pos1, size_t len1, const CHAR_TYPE* str2, size_t pos2, size_t len2)                      \
 {                                                                                                                                                                                           \
     _C_CUSTOM_ASSERT(   pos1 + len1 <= _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, length)(str1) &&                                                                                           \
                         pos2 + len2 <= _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, length)(str2),                                                                                             \
@@ -69,7 +69,7 @@ static int _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_compare)(const CHAR_
 	return _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, compare)(str1 + pos1, str2 + pos2, len1);  /* same length */                                                                           \
 }                                                                                                                                                                                           \
                                                                                                                                                                                             \
-static size_t _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_find)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len)                                                  \
+static size_t _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, find_substr)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len)                                                  \
 {                                                                                                                                                                                           \
     /* search in [str1 + pos, end) the string [str2, str2 + len] */                                                                                                                         \
     _C_CUSTOM_ASSERT(   pos <= _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, length)(str1) &&                                                                                                   \
@@ -81,7 +81,7 @@ static size_t _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_find)(const CHAR_
     return NPOS;                                                                                                                                                                            \
 }                                                                                                                                                                                           \
                                                                                                                                                                                             \
-static size_t _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, substr_rfind)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len)                                                 \
+static size_t _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, rfind_substr)(const CHAR_TYPE* str1, const CHAR_TYPE* str2, size_t pos, size_t len)                                                 \
 {                                                                                                                                                                                           \
 	/* search in [str1, str1 + pos) the string [str2, str2 + len] from right to left */                                                                                                     \
     _C_CUSTOM_ASSERT(   pos <= _C_PUBLIC_MEMBER(CHAR_TRAITS_NAME_PREFIX, length)(str1) &&                                                                                                   \
